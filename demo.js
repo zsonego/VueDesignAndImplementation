7259a024d824a1556c8691932725260a565019e7
timeless <zs_timeless@163.com>
2024-03-05 23:11:12 +0800

    修改

--- demo.js
+++ demo.js
@@ -1,27 +1,35 @@
-const jobQueue = new Set();
-const p = Promise.resolve();
-let isFlushing = false;
+// const jobQueue = new Set();
+// const p = Promise.resolve();
+// let isFlushing = false;
 
-function flushJob() {
-  if (isFlushing) return;
-  isFlushing = true;
-  p.then(() => {
-    jobQueue.forEach((job) => job());
-  }).finally(() => {
-    isFlushing = false;
-  });
-}
+// function flushJob() {
+//   if (isFlushing) return;
+//   isFlushing = true;
+//   p.then(() => {
+//     jobQueue.forEach((job) => job());
+//   }).finally(() => {
+//     isFlushing = false;
+//   });
+// }
 
-let number = 1;
-let fn = () => {
-  console.log(number);
+// let number = 1;
+// let fn = () => {
+//   console.log(number);
+// };
+// jobQueue.add(fn);
+// number++;
+// flushJob();
+// jobQueue.add(fn);
+// number++;
+// flushJob();
+// jobQueue.add(fn);
+// number++;
+// flushJob();
+
+const temp = {
+  name: '123',
+  get demo() {
+    console.log(this.name);
+  },
 };
-jobQueue.add(fn);
-number++;
-flushJob();
-jobQueue.add(fn);
-number++;
-flushJob();
-jobQueue.add(fn);
-number++;
-flushJob();
+Reflect.get(temp, 'demo', { name: 'tom' });
